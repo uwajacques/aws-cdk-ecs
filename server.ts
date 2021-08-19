617aecbf518e70635c09acf4d0c1bd491325b8d4
Jacques Uwamungu <jacquesuwamungu@economist.com>
2021-08-19 09:12:33 +0100

    Init

---
+++ server.ts
@@ -0,0 +1,28 @@
+/*
+|--------------------------------------------------------------------------
+| AdonisJs Server
+|--------------------------------------------------------------------------
+|
+| The contents in this file is meant to bootstrap the AdonisJs application
+| and start the HTTP server to accept incoming connections. You must avoid
+| making this file dirty and instead make use of `lifecycle hooks` provided
+| by AdonisJs service providers for custom code.
+|
+*/
+
+import 'reflect-metadata'
+import sourceMapSupport from 'source-map-support'
+import { Ignitor } from '@adonisjs/core/build/standalone'
+
+sourceMapSupport.install({ handleUncaughtExceptions: false })
+
+const server = new Ignitor(__dirname)
+  .httpServer()
+
+server.start()
+  .catch(console.error)
+
+// Without it process won't die in container
+process.on('SIGINT', () => {
+  server.kill(10)
+})
